5d624e9e99b0d311bb30d42500a3c3517705dcbc
Chavier <javiercastrorodriguez2005@gmail.com>
2024-11-07 17:07:38 +0100

    Bucle de impresoras

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world !");
         System.out.println("prueba");
+         int numeroimpresoras = 10;
+        for (int i = 0; i <= numeroDeImpresoras; i++) {
+            System.out.println("Impresora " + i);
+        }
     }
 }

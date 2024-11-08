d3e57ef6f7daae9c87699f80f292acbc04380e6a
Jesus Rodriguez <jrodsan0407@g.educaand.es>
2024-11-08 14:23:17 +0100

    bucle arreglado

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world !");
         System.out.println("prueba");
-         int numeroimpresoras = 10;
-        for (int i = 0; i <= numeroimpresoras; i++) {
+         int numeroimpresoras2 = 10;
+        for (int i = 0; i <= numeroimpresoras2; i++) {
             System.out.println("Impresora " + i);
         }
     }

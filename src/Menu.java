afa85dffeb0081ada76ea526b5b8d3c24e4f6c17
Jose Oteros <oteros19@gmail.com>
2024-11-09 10:03:02 +0100

    layout con estilo

--- src/Menu.java
+++ src/Menu.java
@@ -8,19 +8,27 @@ public class Menu {
         //marco de la ventana
         JFrame frame = new JFrame("Selector de Fichero");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 400);
+        frame.setSize(700, 400);
+        frame.setResizable(false);
         frame.setLocationRelativeTo(null);
+        Font font = new Font("SansSerif", Font.BOLD, 18);
 
         //desplegable
         JComboBox<String> comboBox = new JComboBox<>();
+        comboBox.setPreferredSize(new Dimension(500, 50));
+        comboBox.setFont(font);
 
         //boton imprimir
         JButton btnImprimir = new JButton("Imprimir");
+        btnImprimir.setPreferredSize(new Dimension(300, 50));
+        btnImprimir.setFont(font);
 
         //area de texto
-        JTextArea textArea = new JTextArea(3, 30);
+        JTextArea textArea = new JTextArea();
         textArea.setEditable(false);
         textArea.setBackground(Color.LIGHT_GRAY);
+        textArea.setPreferredSize(new Dimension(600, 150));
+        textArea.setFont(font);
 
         //directorio de ficheros
         File directorio = new File("files");
@@ -36,7 +44,7 @@ public class Menu {
         }
 
         // Acción del botón "Imprimir"
-        btnImprimir.addActionListener( e -> {
+        btnImprimir.addActionListener(e -> {
             String seleccionado = (String) comboBox.getSelectedItem();
             if (seleccionado != null) {
                 textArea.setText("Fichero " + seleccionado + " enviado a la cola de impresión");
@@ -47,9 +55,13 @@ public class Menu {
 
         JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        JSeparator separator = new JSeparator(SwingConstants.HORIZONTAL);
+        separator.setPreferredSize(new Dimension(2,100));
         panel.add(comboBox);
+        panel.add(separator);
         panel.add(btnImprimir);
-        panel.add(new JScrollPane(textArea));
+        panel.add(separator);
+        panel.add(textArea);
         frame.add(panel);
         frame.setVisible(true);
     }

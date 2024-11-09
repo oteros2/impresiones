a15508d6e55ff3c367daa0526791a8e9a461766e
Jose Oteros <oteros19@gmail.com>
2024-11-09 09:15:08 +0100

    layout

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world !");
-        System.out.println("prueba");
-         int numeroimpresoras = 10;
-        for (int i = 0; i <= numeroDeImpresoras; i++) {
-            System.out.println("Impresora " + i);
-        }
+    public static void main(String[] args){
+        Menu.createMenu();
     }
 }
+
---
+++ src/Menu.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.Arrays;
+
+public class Menu {
+    static void createMenu() {
+        //marco de la ventana
+        JFrame frame = new JFrame("Selector de Fichero");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(200, 400);
+        frame.setLocationRelativeTo(null);
+
+        //desplegable
+        JComboBox<String> comboBox = new JComboBox<>();
+
+        //boton imprimir
+        JButton btnImprimir = new JButton("Imprimir");
+
+        //area de texto
+        JTextArea textArea = new JTextArea(3, 30);
+        textArea.setEditable(false);
+        textArea.setBackground(Color.LIGHT_GRAY);
+
+        //directorio de ficheros
+        File directorio = new File("files");
+        if (directorio.exists() && directorio.isDirectory()) {
+            String[] ficheros = directorio.list();
+
+            if (ficheros != null) {
+                Arrays.sort(ficheros);
+                for (String file : ficheros) {
+                    comboBox.addItem(file);
+                }
+            }
+        }
+
+        // Acción del botón "Imprimir"
+        btnImprimir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String seleccionado = (String) comboBox.getSelectedItem();
+                if (seleccionado != null) {
+                    textArea.setText("Fichero " + seleccionado + " enviado a la cola de impresión");
+                } else {
+                    textArea.setText("No se ha seleccionado ningún fichero.");
+                }
+            }
+        });
+
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        panel.add(comboBox);
+        panel.add(btnImprimir);
+        panel.add(new JScrollPane(textArea));
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+}

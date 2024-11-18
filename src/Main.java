53e3240f6f30a5b89611c9140cffda2b1c56135d
Chavier <javiercastrorodriguez2005@gmail.com>
2024-11-18 16:21:32 +0100

    Creacion de las tres impresoras en el main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args){
         Menu.createMenu();
+        ConcurrentLinkedQueue<TrabajoImpresion> colaDeImpresion = new ConcurrentLinkedQueue<>();
+        int limiteImpresiones = 5;
+        Thread impresora1 = new Thread(new Impresoras(colaDeImpresion, limiteImpresiones), "Impresora 1");
+        Thread impresora2 = new Thread(new Impresoras(colaDeImpresion, limiteImpresiones), "Impresora 2");
+        Thread impresora3 = new Thread(new Impresoras(colaDeImpresion, limiteImpresiones), "Impresora 3");
+
+        impresora1.start();
+        impresora2.start();
+        impresora3.start();
     }
 }
 

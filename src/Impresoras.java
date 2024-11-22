fe624dd461233645f8cfacc654df7dd6e5d40279
Chavier <javiercastrorodriguez2005@gmail.com>
2024-11-22 09:45:42 +0100

    Codigo mas optimizado para que tenga mejor funcionalidad y añadida funcion de que las ventanas se cierren automaticamente en cierto punto

--- src/Impresoras.java
+++ src/Impresoras.java
@@ -19,56 +19,70 @@ class Impresoras implements Runnable {
 
     @Override
     public void run() {
-        Random random = new Random();
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         while (true) {
             if (contadorImpresiones < limiteImpresiones) {
                 TrabajoImpresion trabajo = colaDeImpresion.poll(); //intenta extraer un documentode la cola y en caso contrario espera un breve tiempo
                 if (trabajo != null) {
+                    JOptionPane option = new JOptionPane("", JOptionPane.INFORMATION_MESSAGE);
+                    StringBuilder impresion = new StringBuilder();
                     try (BufferedReader br = new BufferedReader(new FileReader(trabajo.getArchivo()))) {
                         System.out.println(Thread.currentThread().getName() + " está imprimiendo: " + trabajo.getNombreArchivo());
                         String linea;
                         while ((linea = br.readLine()) != null) {
-                            int width = screenSize.width;
-                            int height = screenSize.height;
-                            int x = random.nextInt(width - 300);
-                            int y = random.nextInt(height - 200);
-                            JFrame frame = new JFrame();
-                            frame.setUndecorated(true);
-                            frame.setBounds(x, y, 1, 1);
-                            frame.setVisible(true);
-                            JOptionPane.showMessageDialog(frame, Thread.currentThread().getName() + ": " + linea);
+                            impresion.append(linea + "\n");
                         }
+                        option.setMessage(impresion);
+                        JDialog dialog = option.createDialog(Thread.currentThread().getName());
+                        int screenWidth = (int) java.awt.Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+                        int screenHeight = (int) java.awt.Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+                        int randomX = (int) (Math.random() * (screenWidth - dialog.getWidth()));
+                        int randomY = (int) (Math.random() * (screenHeight - dialog.getHeight()));
+                        dialog.setLocation(randomX, randomY);
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    Thread.sleep(1000); // Simula el tiempo de impresión
+                                } catch (InterruptedException e) {
+                                    throw new RuntimeException(e);
+                                }
+                                dialog.setVisible(false);
+                            }
+                        }).start();
+                        dialog.setVisible(true);
+                        dialog.dispose();
                         contadorImpresiones++;
-                    } catch (IOException e) {
+                    }catch  (InterruptedException | IOException e) {
                         throw new RuntimeException(e);
                     }
                     try {
-                        Thread.sleep(1000); // Simula el tiempo de impresión
-                    } catch (InterruptedException e) {
+                        Thread.sleep(1000);
+                    }catch (InterruptedException e) {
                         Thread.currentThread().interrupt();
+                    }
+                    } else{
+                        try {
+                            Thread.sleep(100); // Espera un pequeño intervalo de tiempo si la cola está vacía
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
                     }
                 } else {
+                    System.out.println("[" + Thread.currentThread().getName() + " ha alcanzado su límite de impresiones (" + limiteImpresiones + "). Esperando para reiniciar...]");
+
                     try {
-                        Thread.sleep(100); // Espera un pequeño intervalo de tiempo si la cola está vacía
+                        Random random = new Random();
+                        int tiempoEspera = random.nextInt(11) * 1000;
+                        System.out.println("Tiempo estimado de espera " + tiempoEspera / 1000 + " s");// Genera un número aleatorio entre 0 y 10 (en milisegundos)
+                        Thread.sleep(tiempoEspera);
                     } catch (InterruptedException e) {
                         Thread.currentThread().interrupt();
                     }
-                }
-            } else {
-                System.out.println("["+Thread.currentThread().getName() + " ha alcanzado su límite de impresiones (" + limiteImpresiones + "). Esperando para reiniciar...]");
 
-                try {
-                    int tiempoEspera = random.nextInt(11) * 1000;
-                    System.out.println("Tiempo estimado de espera "+tiempoEspera/1000+" s");// Genera un número aleatorio entre 0 y 10 (en milisegundos)
-                    Thread.sleep(tiempoEspera);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
+                    contadorImpresiones = 0; // Reinicia el contador de impresiones
+                    System.out.println(Thread.currentThread().getName() + " está lista para imprimir nuevamente.");
                 }
-
-                contadorImpresiones = 0; // Reinicia el contador de impresiones
-                System.out.println(Thread.currentThread().getName() + " está lista para imprimir nuevamente.");
             }
         }
     }
-}
+
